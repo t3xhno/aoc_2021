1c3266999f435580aea65f7a7490bbde67470586
tex <t3xhnolainx@gmail.com>
2022-07-23 15:44:27 +0200

    aoc2021 | day01 + day02

---
+++ day01/index.js
@@ -0,0 +1,9 @@
+const R = require("ramda");
+const { s } = require("../lib/combinators");
+const { fetchDataRows } = require("../lib/inputReader");
+
+const solve_1 = (arr) => arr.reduce((acc, curr, i, arr) => i === 0 ? acc : curr > arr[i - 1] ? acc + 1 : acc, 0);
+const makeWindows = (arr) => arr.map((_e, i, a) => a.slice(i, i + 3)).slice(0, -2);
+const solve_2 = R.pipe(makeWindows, R.map(R.sum), solve_1);
+
+R.pipe(fetchDataRows, R.map((d => +d)), s(solve_1, solve_2))("data.txt");
---
+++ day02/index.js
@@ -0,0 +1,16 @@
+const R = require("ramda");
+const { s } = require("../lib/combinators");
+const { fetchDataRows } = require("../lib/inputReader");
+
+const r = /(\w)\w+ (\d+)/;
+const step = ([d, i]) => d.match(/[fd]/) ? +i : -i;
+const parse = (str) => str.match(r).slice(1, 3);
+const compute = (arr) => arr.reduce(([x, y], e) => e[0].match(/[du]/) ? [x, y + step(e)] : [x + step(e), y], [0, 0]);
+const computeWithAim = (arr) => arr.reduce(([x, y, aim], e) =>
+  e[0].match(/[ud]/) ? [x, y, aim + (e[0].match(/d/) ? +e[1] : -e[1])] : [x + +e[1], y + +e[1] * aim, aim]
+, [0, 0, 0]);
+
+const solve_1 = R.pipe(R.map(parse), compute, R.product);
+const solve_2 = R.pipe(R.map(parse), computeWithAim, R.take(2), R.product);
+
+R.pipe(fetchDataRows, s(solve_1, solve_2))("data.txt");
