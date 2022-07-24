41970cfd147ab9ef6a1b0bf03724ea3f72075deb
tex <t3xhnolainx@gmail.com>
2022-07-24 15:01:39 +0200

    aoc2021 | Using R.pluck instead of map.prop

--- day08/index.js
+++ day08/index.js
@@ -4,6 +4,13 @@ const { tLog, fetchDataRows, S } = require("../lib/lib");
 const easyDigitLengths = /[2347]/;
 const isEasyDigit = (num) => num.toString().match(easyDigitLengths);
 
-const solve_1 = R.pipe(R.map(R.split(/\s\|\s/)), R.map(R.prop(1)), R.map(R.split(/\s/)), R.flatten, R.map(R.length), R.filter(isEasyDigit), R.length);
+const solve_1 = R.pipe(
+  R.map(R.split(/\s\|\s/)),
+  R.pluck(1),
+  R.map(R.split(/\s/)),
+  R.flatten,
+  R.map(R.length),
+  R.filter(isEasyDigit),
+  R.length);
 
 R.pipe(fetchDataRows, S(solve_1), tLog)("data.txt");
