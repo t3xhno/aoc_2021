1eb6a248e76d80ead13672edc33357a720ffa508
tex <t3xhnolainx@gmail.com>
2022-07-24 14:59:44 +0200

    aoc2021 | Added day08.1

---
+++ day08/index.js
@@ -0,0 +1,9 @@
+const R = require("ramda");
+const { tLog, fetchDataRows, S } = require("../lib/lib");
+
+const easyDigitLengths = /[2347]/;
+const isEasyDigit = (num) => num.toString().match(easyDigitLengths);
+
+const solve_1 = R.pipe(R.map(R.split(/\s\|\s/)), R.map(R.prop(1)), R.map(R.split(/\s/)), R.flatten, R.map(R.length), R.filter(isEasyDigit), R.length);
+
+R.pipe(fetchDataRows, S(solve_1), tLog)("data.txt");
