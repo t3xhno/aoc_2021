1a2c9dc38dd4dd2520f3e64a5605438e4cc01999
tex <t3xhnolainx@gmail.com>
2022-07-24 14:05:53 +0200

    aoc2021 | day05 more expressive naming

--- day05/index.js
+++ day05/index.js
@@ -1,8 +1,8 @@
 const R = require("ramda");
-const { tLog, fetchDataRows, S, F } = require("../lib/lib");
+const { tLog, fetchDataRows, F } = require("../lib/lib");
 
 const parseRow = (str) => str.trim().match(/((?<x1>\d+),(?<y1>\d+)) -> ((?<x2>\d+),(?<y2>\d+))/).groups;
-const isHV = ({ x1, x2, y1, y2 }) => x1 === x2 || y1 === y2;
+const isHorizontalOrVertical = ({ x1, x2, y1, y2 }) => x1 === x2 || y1 === y2;
 const maxX = (arr) => Math.max(...arr.map(({ x1, x2 }) => Math.max(x1, x2)));
 const maxY = (arr) => Math.max(...arr.map(({ y1, y2 }) => Math.max(y1, y2)));
 const joinSize = (x, y) => [x, y];
@@ -14,17 +14,16 @@ const makeMatrix = ([[sizeX, sizeY], lines]) => lines.reduce((acc, { x1, x2, y1,
 , new Array(sizeX + 1).fill(new Array(sizeY + 1).fill(0)));
 
 const solve_1 = R.pipe(
-  R.identity,
   R.map(parseRow),
-  R.filter(isHV),
+  R.filter(isHorizontalOrVertical),
   F(joinAll, F(joinSize, maxX, maxY), R.identity),
   makeMatrix,
   R.flatten,
-  R.filter(x => x >= 2),
+  R.filter(R.lte(2)),
   R.length);
 
 const solve_2 = R.pipe(
   R.identity,
 );
 
-R.pipe(fetchDataRows, S(solve_1), tLog)("data.txt");
+R.pipe(fetchDataRows, solve_1, tLog)("data.txt");
