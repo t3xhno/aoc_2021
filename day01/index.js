6aeadf7f033ce9f5a8df686938e86e03bb422165
tex <t3xhnolainx@gmail.com>
2022-07-24 14:17:34 +0200

    aoc2021 | Added logging to day01

--- day01/index.js
+++ day01/index.js
@@ -1,8 +1,8 @@
 const R = require("ramda");
-const { fetchDataRows, S } = require("../lib/lib");
+const { tLog, fetchDataRows, S } = require("../lib/lib");
 
 const solve_1 = (arr) => arr.reduce((acc, curr, i, arr) => i === 0 ? acc : curr > arr[i - 1] ? acc + 1 : acc, 0);
 const makeWindows = (arr) => arr.map((_e, i, a) => a.slice(i, i + 3)).slice(0, -2);
 const solve_2 = R.pipe(makeWindows, R.map(R.sum), solve_1);
 
-R.pipe(fetchDataRows, R.map((d => +d)), S(solve_1, solve_2))("data.txt");
+R.pipe(fetchDataRows, R.map((d => +d)), S(solve_1, solve_2), tLog)("data.txt");
